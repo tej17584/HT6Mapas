45bca4868f44f4071782d21b1b86e33cd9d1bdba
José Alejandro Tejada Flores <tej17584@uvg.edu.gt>
2018-03-08 14:50:24 -0600

    Cambios en el main

--- HojaDeTrabajo6/src/MainLector.java
+++ HojaDeTrabajo6/src/MainLector.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -22,7 +23,7 @@ public class MainLector {
       File archivo = null;
       FileReader fr = null;
       BufferedReader br = null;
-
+Scanner entrada = new Scanner(System.in);
       try {
          // Apertura del fichero y creacion de BufferedReader para poder
          // hacer una lectura comoda (disponer del metodo readLine()).
@@ -59,7 +60,75 @@ public class MainLector {
          }catch (IOException e2){ 
          }
       }
-        
+          
+    System.out.println("indique el tipo de implementacion que desea utilizar\n");
+    System.out.println("1.HashMap \n 2.TreeMap \n 3.LinkedHashMap \n");
+    int opcion1 = 0;
+    String opcionS;
+    boolean continuar = true;
+    while(continuar ==true){
+    opcionS = entrada.nextLine();
+    opcion1 = Integer.parseInt(opcionS);
+        try{
+            
+            if(opcion1==1){
+            System.out.println("Selecciono Hasmap como implementacion"
+                    + ""); 
+                
+                
+                
+            }
+            if(opcion1==2){
+            System.out.println("Selecciono Treemap como implementacion"
+                    + "");    
+                
+                
+                
+            }
+            if(opcion1==3){
+            System.out.println("Selecciono LinkedHasmap como implementacion"
+                    + "");    
+                
+                
+                
+            }
+        int opcion2;
+        System.out.println("1. Agregar una carta a la colección del usuario. Para esto el usuario ingresa el nombre de la carta que desea agregar a la misma.\n"
+        +"2. Mostrar el tipo de una carta específica. El usuario ingresará el nombre de la carta y se muestra el tipo de esa carta.\n"
+        +"3. Mostrar el nombre, tipo y cantidad de cada carta que el usuario tiene en su colección.\n"
+        +"4. Mostrar el nombre, tipo y cantidad de cada carta que el usuario tiene en su colección, ordenadas por tipo.\n"
+        +"5. Mostrar el nombre y tipo de todas las cartas existentes.\n"
+        +"6. Mostrar el nombre y tipo de todas las cartas existentes, ordenadas por tipo.");
+        opcionS = entrada.nextLine();
+        opcion2 = Integer.parseInt(opcionS);
+            if(opcion2==1){
+                
+            }
+            if(opcion2==2){
+                
+            }
+            if(opcion2==3){
+                
+            }
+            if(opcion2==4){
+                
+            }
+            if(opcion2==5){
+                
+            }
+            if(opcion2==6){
+                
+            }
+            if(opcion2==7){
+                System.out.println("Gracias por usar el programa");
+                continuar = false;
+            }
+            
+        }
+        catch (Exception e) {
+            System.out.println("Esa opcion no esta disponible o no ingreso el numero de la opcion");
+        }
+    }  
         
     }
 }
