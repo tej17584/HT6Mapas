61714bb5a0a23fe783b0a49a25ca09cce374f52a
José Alejandro Tejada Flores <tej17584@uvg.edu.gt>
2018-03-08 20:23:39 -0600

    Métodos en operaciones

--- HojaDeTrabajo6/src/Operaciones.java
+++ HojaDeTrabajo6/src/Operaciones.java
@@ -1,5 +1,6 @@
 
 import java.util.HashMap;
+import java.util.Map;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,8 +13,46 @@ import java.util.HashMap;
  * @author Jose Tejada
  */
 public class Operaciones {
-   protected Operaciones miOp;
-   MapFactory mapFC= new MapFactory();
+   Map<String,String> mapaOp;
+   
+   public Operaciones(){
+       
+   }
+   
+   public void setMapa(Map<String,String> mapa){
+       this.mapaOp=mapa;
+   }
+   
+   public Map<String,String> getMapa(){
+       return this.mapaOp;
+   }
+   
+   public boolean validarValue(String Value){
+       boolean retorno=false;
+       if (this.mapaOp.containsValue(Value)==true) {
+           retorno=true;
+       }
+       else if (this.mapaOp.containsValue(Value)==false) {
+           retorno=false;
+           
+       }
+       return retorno;
+   }
+   
+   public boolean validarKey(String Key){
+       boolean retorno=false;
+       if(this.mapaOp.containsKey(Key)==true){
+           retorno=true;
+       }
+       else if (this.mapaOp.containsKey(Key)==false) {
+           retorno=false;
+       }
+       return retorno;
+   }
+   
+   public void agregarCarta(String Key, String Value){
+       
+   }
    
  
 }
