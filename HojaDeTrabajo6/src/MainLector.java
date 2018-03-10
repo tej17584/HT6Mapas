18aa0f2edb0373753e3af901ee18a5c1fb1ce767
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2018-03-09 22:54:30 -0600

    Ejercicios 4 y 6 ademas de documentacion

--- HojaDeTrabajo6/src/MainLector.java
+++ HojaDeTrabajo6/src/MainLector.java
@@ -11,6 +11,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.InputMismatchException;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -19,6 +20,7 @@ import java.util.TreeMap;
 /**
  *
  * @author Jose Tejada
+ * @author Guillermo Sandoval
  */
 public class MainLector {
     public void leerYEnviar(Map<String,String> mapaEnviar){
@@ -59,55 +61,103 @@ public class MainLector {
       }
     }
    
-       public static void main(String[] arg) {
+public static void main(String[] arg) {
       
-      Map<String,String> mapaEnviar;
-     MainLector main= new MainLector();
-     Operaciones MiOperacion = null;
-     MapFactory miFC= new MapFactory();
-Scanner entrada = new Scanner(System.in);
+    Map<String,String> mapaEnviar = null;
+    MainLector main= new MainLector();
+    Operaciones MiOperacion = null;
+    MapFactory miFC= new MapFactory();
+    Scanner entrada = new Scanner(System.in);
     
-          
-    System.out.println("indique el tipo de implementacion que desea utilizar\n");
-    System.out.println("1.HashMap \n 2.TreeMap \n 3.LinkedHashMap \n");
-    
-    int opcion1 = 0;
-    String opcionS;
     boolean continuar = true;
+    boolean continuar2=true;
+    
     while(continuar ==true){
-    opcionS = entrada.nextLine();
-    opcion1 = Integer.parseInt(opcionS);
+        //Un while para el menu que tiene metodo de seguridad contra valores fuera de rango
+        //Y contra letras que pueda insertar el usuario 
         try{
-            
+            System.out.println("indique el tipo de implementacion que desea utilizar\n");
+            System.out.println("1.HashMap \n 2.TreeMap \n 3.LinkedHashMap ");
+            int opcion1 = entrada.nextInt();
             if(opcion1==1){
                 
                 mapaEnviar=miFC.getImplementacion(opcion1);
                 main.leerYEnviar(mapaEnviar);
+                continuar2 =true;
                
             }
-            if(opcion1==2){
+            else if(opcion1==2){
                 
                 mapaEnviar=miFC.getImplementacion(opcion1);
                 main.leerYEnviar(mapaEnviar);
+                continuar2 =true;
             }
-            if(opcion1==3){
+            else if(opcion1==3){
                  
                 mapaEnviar=miFC.getImplementacion(opcion1);
                 main.leerYEnviar(mapaEnviar);
+                continuar2 =true;
             }
-        int opcion2;
-        System.out.println("1. Agregar una carta a la colección del usuario. Para esto el usuario ingresa el nombre de la carta que desea agregar a la misma.\n"
-        +"2. Mostrar el tipo de una carta específica. El usuario ingresará el nombre de la carta y se muestra el tipo de esa carta.\n"
-        +"3. Mostrar el nombre, tipo y cantidad de cada carta que el usuario tiene en su colección.\n"
-        +"4. Mostrar el nombre, tipo y cantidad de cada carta que el usuario tiene en su colección, ordenadas por tipo.\n"
-        +"5. Mostrar el nombre y tipo de todas las cartas existentes.\n"
-        +"6. Mostrar el nombre y tipo de todas las cartas existentes, ordenadas por tipo.");
+            else if(opcion1<1 || opcion1>3){
+                System.out.println("Esa opcion no es valida, solo hay entre 1 y 3\n");
+                continuar2 =false;
+            }
+        }
+        catch (InputMismatchException e){
+            System.out.println("Ingreso una o varias letras, vuelva a intentarlo");
+            continuar2 =false;
+            break;
+        }
+        
+        while(continuar==true && continuar2==true){
+            System.out.println("Ingrese el numero de la opcion que desea");
+            System.out.println("1. Agregar una carta a la colección del usuario. Para esto el usuario ingresa el nombre de la carta que desea agregar a la misma.\n"
+            +"2. Mostrar el tipo de una carta específica. El usuario ingresará el nombre de la carta y se muestra el tipo de esa carta.\n"
+            +"3. Mostrar el nombre, tipo y cantidad de cada carta que el usuario tiene en su colección.\n"
+            +"4. Mostrar el nombre, tipo y cantidad de cada carta que el usuario tiene en su colección, ordenadas por tipo.\n"
+            +"5. Mostrar el nombre y tipo de todas las cartas existentes.\n"
+            +"6. Mostrar el nombre y tipo de todas las cartas existentes, ordenadas por tipo.\n"
+            +"7. Salir");
       
-        opcionS = entrada.nextLine();
-        opcion2 = Integer.parseInt(opcionS);
-            if(opcion2==1){
-               System.out.println("OPCION 1");
-               Operaciones.agregarCarta("Texas Spell", "Hechizo");
+            
+            try{
+                //El segundo menu luego de usar implementacion con igual cantidad de progra defensiva al anterior
+                int opcion2 = entrada.nextInt();
+                if(opcion2==1){
+                    System.out.println("OPCION 1");
+               
+                    String opcionS2;
+                    String tipoS = null;
+               
+                    System.out.println("Escriba el nombre de su carta:");
+                    System.out.println("");
+                    //Por alguna razon si solo hay un entrada.nextLine no lee esto asi que se tuvo que usar doble
+                    String cartaS = entrada.nextLine();
+                    String cartaS1 = entrada.nextLine();
+               
+                try{
+                    System.out.println("Escoja el tipo de su carta (NUMERO): \n 1.Monstruo \n 2. Hechizo \n 3. Trampa");
+                    int opcion3 = entrada.nextInt();
+                    if(opcion3==1){
+                        System.out.println("Su carta es un Monstruo");
+                        tipoS = "Monstruo";
+               }
+                    if(opcion3==2){
+                        System.out.println("Su carta es un hechizo");
+                        tipoS = "Hechizo";
+               }
+                    if(opcion3==3){
+                        System.out.println("Su carta es una trampa");
+                        tipoS = "Trampa";
+               }
+                    else if (opcion3<1 || opcion3>3){
+                        System.out.println("Eso no esta entre las opciones, solo de 1 a 3\n");
+                    }
+               }catch(Exception e){
+                   System.out.println("Esa opcion no esta disponible, escoja otra\n");
+               }
+               System.out.println("La carta: "+cartaS1+" del tipo: "+ tipoS +" Ha sido agregada");
+               Operaciones.agregarCarta(cartaS1, tipoS);
             }
             if(opcion2==2){
                 System.out.println("OPCION 2");
@@ -118,24 +168,33 @@ Scanner entrada = new Scanner(System.in);
                 
             }
             if(opcion2==4){
-                
+                //Devuelve el resultado tanto para abajo como en linea por un pequeño error pero si lo arregla
+                Map<String,String> mazo;
+                mazo = Operaciones.setCantMazo(mapaEnviar);
+                System.out.println(mazo);
             }
             if(opcion2==5){
                 Operaciones.imprimirCartas();
             }
             if(opcion2==6){
-                
+                //Devuelve el resultado tanto para abajo como en linea por un pequeño error pero si lo arregla
+                Map<String,String> mazo;
+                mazo = Operaciones.inciso6(mapaEnviar);
+                System.out.println(mazo);
             }
             if(opcion2==7){
                 System.out.println("Gracias por usar el programa");
                 continuar = false;
             }
-            
+            else if(opcion2<1 || opcion2>7){
+            System.out.println("Esa opcion no es valida, solo hay entre 1 y 7\n");
         }
-        catch (NumberFormatException e) {
-            System.out.println("Esa opcion no esta disponible o no ingreso el numero de la opcion");
         }
-    }  
-        
+        catch (InputMismatchException e) {
+            System.out.println("Esa opcion no esta disponible o no ingreso el numero de la opcion\n");
+            break;
+        }
+        }      
     }
+       }
 }
--- HojaDeTrabajo6/src/Operaciones.java
+++ HojaDeTrabajo6/src/Operaciones.java
@@ -1,34 +1,86 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
- * Clase de Operaciones para las selecciones del Usuario
- * @author Jose Tejada
  * @author Guillermo Sandoval
- * @version 1.0
+ * @author Jose Tejada
  */
 public class Operaciones {
    static   Map<String,String> mapaOp;
   
    public Operaciones(){
-     
+    //Constructor del codigo
    }
    
    public static void setMapa(Map<String,String> mapa){
-       System.out.println("Mapa ingreado");
+    //Para construir un mapa Hash con key y value
+       System.out.println("Mapa ingresado");
        Operaciones.mapaOp=mapa;
        
    }
-   
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> setCantMazo(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+        Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return (o1.getValue()).compareTo( o2.getValue() );
+            }
+        });
+        Map<K, V> result = new LinkedHashMap<K, V>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        
+       Iterator<String> it = Operaciones.mapaOp.keySet().iterator();
+       
+       int monstruo = 0;
+       int hechizo=0;
+       int trampa=0;
+        while(it.hasNext()){
+        String key = it.next();
+           switch (Operaciones.mapaOp.get(key)) {
+               case "Monstruo":
+                   monstruo++;
+                   break;
+               case "Hechizo":
+                   hechizo++;
+                   break;
+               case "Trampa":
+                   trampa++;
+                   break;
+               default:
+                   break;
+           }
+   }
+        String linea ="";
+        for (Map.Entry<K, V> entryCol : result.entrySet()){
+            linea += "Nombre Carta: " + entryCol.getKey()+"  ->  Tipo:"+ entryCol.getValue() + "\n";
+        }
+        System.out.println(linea);
+        System.out.println("TOTAL MONSTRUO: "+monstruo + " TOTAL HECHIZOS: "+hechizo + " TOTAL TRAMPAS: "+ trampa);
+    return result;
+    }
    public static Map<String,String> getMapa(){
        return Operaciones.mapaOp;
    }
    
    public static  boolean validarValue(String Value){
+       //Nos sirve para validar si un valor se encuentra al ir a buscarlo o no
+       //Es booleano porque indica si el valor existe (verdadero) o si no existe (falso)
        boolean retorno=false;
        if (Operaciones.mapaOp.containsValue(Value)==true) {
            retorno=true;
@@ -41,6 +93,7 @@ public class Operaciones {
    }
    
    public static boolean validarKey(String Key){
+       //Nos sirve para verificar si contiene esa llave, es decir si existe, lo mismo que validarValue pero con la llave
        boolean retorno=false;
        if(Operaciones.mapaOp.containsKey(Key)==true){
            retorno=true;
@@ -52,10 +105,11 @@ public class Operaciones {
    }
    
    public static void agregarCarta(String Key, String Value){
+       //Lo que hace es validar si existe un valor que ya exista para limitar que solo esten Monstruo, Hechizo y Trampa
+       //Y ademas verifica que la llave no este ocupada, sino nos da error porque tal carta ya existiria
        if ((validarValue(Value)==true)&&(validarKey(Key)==false)) {
            Operaciones.mapaOp.put(Key, Value);
-         
-          
+
            System.out.println("Carta Ingresada con éxito");
        }
        else if ((validarKey(Key)==true)&&(validarValue(Value)==true)) {
@@ -79,6 +133,9 @@ public class Operaciones {
    }
    
    public static void imprimirInciso3(){
+       //El inciso 3 nos pide saber cuantas cartas hay de cada tipo, para ello se uso un iterador
+       //Al iniciar todo se encuentra en 0 y el iterador recorre por llave el valor, si detecta que el valor
+       //Ya se encoontraba antes les suma uno al valor y asi sigue recorriendo toda la lista de cartas.
        Iterator<String> it = Operaciones.mapaOp.keySet().iterator();
        int monstruo = 0;
        int hechizo=0;
@@ -104,6 +161,7 @@ public class Operaciones {
    }
    
    public static void imprimirCartas(){
+       //Simplemente pasa por todo el mapa e imprime todos los valores junto a sus llaves unicas
        System.out.println("TODAS LAS CARTAS: ");
         Iterator<String> it = Operaciones.mapaOp.keySet().iterator();
         while(it.hasNext()){
@@ -111,4 +169,32 @@ public class Operaciones {
           System.out.println("Clave: " + key + " -> Valor: " + Operaciones.mapaOp.get(key));
         }
    }
+   public static <K, V extends Comparable<? super V>> Map<K, V> inciso6(Map<K, V> map){
+        //En el inciso 6 se nos pide ordenar las cartas por su tipo
+        //La forma mas facil era solamente imprimiendo primero todas las de valor Monstruo
+        //Luego las de valor Hechizo y luego Trampa.
+        //Pero para que quede ordenado alphabeticamente por valor decidimos usar el codigo de 
+        //sorting que emplea comparable, esto se encontro en StackOverflow pero vamos a explicarlo
+        List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+        //Para comparar el comparable lo que hace es como valores, si el valor de 1 es menor al de 2 va arriba
+        //Nos regresa la comparacion del valor, la forma de hacerlo es por medio de decir si el tipo de la carta
+        //ya esta, por sus letras, por ejemplo Hechizo, la H es previa a la M y a la T, por lo que todos los Hechizos
+        //Van de primero.
+        Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+            public int compare(Map.Entry<K, V> valor1, Map.Entry<K, V> valor2) {
+                return (valor1.getValue()).compareTo( valor2.getValue() );
+            }
+        });
+        Map<K, V> result = new LinkedHashMap<K, V>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        String linea ="";
+        for (Map.Entry<K, V> entryCol : result.entrySet()){
+            linea += "Nombre Carta: " + entryCol.getKey()+"  ->  Tipo:"+ entryCol.getValue() + "\n";
+        }
+        System.out.println(linea);
+        return result;
+   }
 }
+
